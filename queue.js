bbda2862c09e0e9d9411495eb0730a8f39c7174f
angorafish <mimitunafish@gmail.com>
2024-05-16 12:57:57 -0700

    Commit

---
+++ queue.js
@@ -0,0 +1,37 @@
+const LinkedList = require('./LinkedList');
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this._list = new LinkedList();
+  }
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+
+  enqueue(val) {
+    this._list.addLast(val);
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+
+  dequeue() {
+    return this._list.removeFirst();
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {
+    return this._list.getFirst();
+  }
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {
+    return this._list.isEmpty();
+  }
+}
+
+module.exports = Queue;
